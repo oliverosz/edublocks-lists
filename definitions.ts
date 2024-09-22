9789db02a002e7e2ac8bdae8b2c1c56811a17d28
Olivér Ősz <16599031+oliverosz@users.noreply.github.com>
2024-09-22 10:44:14 +0200

    Add fixed append

--- definitions.ts
+++ definitions.ts
@@ -16,3 +16,18 @@ Blockly.Blocks['list_item_assign'] = {
         this.setColour(listsColor);
     }
 };
+
+Blockly.Blocks['list_append_fixed'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField(new Blockly.FieldVariable("variable_name"), "list_name_before_append")
+            .appendField(".append(");
+        this.appendValueInput("list_append_arg");
+        this.appendDummyInput()
+            .appendField(")");
+        this.setInputsInline(true);
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(listsColor);
+    }
+};
--- generators.ts
+++ generators.ts
@@ -6,3 +6,10 @@ Blockly.Python['list_item_assign'] = function(block) {
     const code = `${list_name}[${idx}] ${op} ${val}\n`;
     return code;
 };
+
+Blockly.Python['list_append_fixed'] = function(block) {
+    const list_name = Blockly.Python.nameDB_.getName(block.getFieldValue('list_name_before_append'), Blockly.VARIABLE_CATEGORY_NAME);
+    const arg = Blockly.Python.valueToCode(block, 'list_append_arg', 0);
+    const code = `${list_name}.append(${arg})\n`;
+    return code;
+};
