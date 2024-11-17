9394530ce6ae3828f4b15a13697ece337aff4d00
Olivér Ősz <16599031+oliverosz@users.noreply.github.com>
2024-11-17 19:25:26 +0100

    Add matrix_item_assign

--- definitions.ts
+++ definitions.ts
@@ -16,3 +16,23 @@ Blockly.Blocks['list_item_assign'] = {
         this.setColour(listsColor);
     }
 };
+
+Blockly.Blocks['matrix_item_assign'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField(new Blockly.FieldVariable("variable_name"), "list_name")
+            .appendField("[");
+        this.appendValueInput("idx1");
+        this.appendDummyInput()
+            .appendField("][");
+        this.appendValueInput("idx2");
+        this.appendDummyInput()
+            .appendField("]")
+            .appendField(new Blockly.FieldDropdown([["=","="], ["+=","+="], ["-=","-="]]), "op");
+        this.appendValueInput("val");
+        this.setInputsInline(true);
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(listsColor);
+    }
+};
